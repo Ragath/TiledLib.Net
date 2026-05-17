22a1ba74c8eed847fa40a11c6a7e0822f6457e24
Daniel Sör <Ragath@users.noreply.github.com>
2026-05-17 19:16:19 +0200

    Add chests tileset, images, and new Tiled map files.

---
+++ TiledLib.Tests/Data/Chests.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Chests" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="chest_0.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="chest_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="chest_2.png" width="32" height="32"/>
+ </tile>
+</tileset>
